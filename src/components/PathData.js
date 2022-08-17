72e2f36d7761983326b002ddf726f21e069e370f
Prajituric <bugheanudanutandrei@gmail.com>
2022-08-17 17:17:32 +0300

    new project

---
+++ src/components/PathData.js
@@ -0,0 +1,79 @@
+export const Paths = [
+  {
+    id: 1,
+    name: "Starting to find more about Web Development and being attracted to Frontend",
+    date: "Jun, 2020",
+    imgSrc:
+      "https://res.cloudinary.com/practicaldev/image/fetch/s--sBeV06Xc--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/2yrzy75q4dzibqz6rf82.png",
+  },
+  {
+    id: 2,
+    name: "Begin to learn HTML and CSS",
+    tags: ["react", "styled-components"],
+    date: "Aug, 2020",
+    imgSrc:
+      "https://codebucks.hashnode.dev/_next/image?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Fupload%2Fv1625479747640%2F7KzwP9nmj.png%3Fw%3D1600%26h%3D840%26fit%3Dcrop%26crop%3Dentropy%26auto%3Dcompress%2Cformat%26format%3Dwebp&w=1920&q=75",
+    link: "https://codebucks.hashnode.dev/react-hamburger-menu",
+  },
+  {
+    id: 3,
+    name: "Finding and learning about JavaScript",
+    tags: ["react", "react-lottie", "styled-components"],
+    date: "Dec, 2020",
+    imgSrc:
+      "https://codebucks.hashnode.dev/_next/image?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Fupload%2Fv1625552344293%2Fegwis0UIX.png%3Fw%3D1600%26h%3D840%26fit%3Dcrop%26crop%3Dentropy%26auto%3Dcompress%2Cformat%26format%3Dwebp&w=1920&q=75",
+    link: "https://codebucks.hashnode.dev/react-loading-screens",
+  },
+  {
+    id: 4,
+    name: "Getting into UX/UI design",
+    tags: ["react", "redux", "framer-motion"],
+    date: "May, 2021",
+    imgSrc:
+      "https://res.cloudinary.com/practicaldev/image/fetch/s--r5oUDhhT--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/036s7j5b73up7455rdth.png",
+    link: "https://dev.to/codebucks/build-redux-react-todo-list-app-with-animations-using-framer-motion-1mp1",
+  },
+  {
+    id: 5,
+    name: "Going deeper into Cotrol Systems like Git and GitHub",
+    date: "July, 2021",
+    imgSrc:
+      "https://codebucks.hashnode.dev/_next/image?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Fupload%2Fv1625295480148%2FlyKj8bpHK.jpeg%3Fw%3D1600%26h%3D840%26fit%3Dcrop%26crop%3Dentropy%26auto%3Dcompress%2Cformat%26format%3Dwebp&w=1920&q=75",
+  },
+  {
+    id: 6,
+    name: "Learning about package managers like NPM, CSS Processors and Frameworks",
+    date: "Aug, 2021",
+    imgSrc:
+      "https://codebucks.hashnode.dev/_next/image?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Fupload%2Fv1625240050668%2FGAya9b0XK.png%3Fw%3D1600%26h%3D840%26fit%3Dcrop%26crop%3Dentropy%26auto%3Dcompress%2Cformat%26format%3Dwebp&w=1920&q=75",
+  },
+
+  {
+    id: 7,
+    name: "Going further into React, TypeScript and Testing with Jest",
+    date: "Sept, 2021",
+    imgSrc:
+      "https://res.cloudinary.com/practicaldev/image/fetch/s--ZY1EK-Eo--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/bcz1u90oq3prr5fga3fs.png",
+  },
+  {
+    id: 8,
+    name: "Getting into Computer Science College",
+    date: "Oct, 2021",
+    imgSrc:
+      "https://res.cloudinary.com/practicaldev/image/fetch/s--rv4I8UwE--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/0a9h50milhlf1us6mn4a.png",
+  },
+  {
+    id: 9,
+    name: "Enroll into a Frontend Bootcamp, finishing until now multiple courses on Udemy, FreeCodeCamp and Harvard CS50",
+    date: "Feb, 2022",
+    imgSrc:
+      "https://res.cloudinary.com/practicaldev/image/fetch/s--rv4I8UwE--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/0a9h50milhlf1us6mn4a.png",
+  },
+  {
+    id: 10,
+    name: "Continuing learning something new everyday",
+    date: "Today",
+    imgSrc:
+      "https://res.cloudinary.com/practicaldev/image/fetch/s--rv4I8UwE--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/0a9h50milhlf1us6mn4a.png",
+  },
+];
---
+++ src/components/WorkData.js
@@ -0,0 +1,62 @@
+export const Work = [
+  {
+    id: 1,
+    name: "Travelly",
+    description:
+      "It is build as a prototype for a travel app. Based on css and html to combine the useful with the pleasant.",
+    tags: ["html", "css", "travel", "animations"],
+
+    demo: "https://travelwithlove.netlify.app/",
+    github: "https://github.com/Prajituric/Travelly",
+  },
+  {
+    id: 2,
+    name: "Photography",
+    description:
+      "A website which combine the photographer with the beauty of his art.",
+    tags: ["minimalism", "css"],
+
+    demo: "https://photographerantonia.netlify.app/",
+    github: "https://github.com/Prajituric/Photography-site",
+  },
+  {
+    id: 3,
+    name: "News Paper",
+    description:
+      "A template for a news page which combine the style with multiple informations",
+    tags: ["news", "css", "grid"],
+
+    demo: "https://whispernow.netlify.app/index.html",
+    github: "https://github.com/Prajituric/Whisper-News",
+  },
+  // {
+  //   id: 4,
+  //   name: "Sidebar Menu",
+  //   description:
+  //     "A cool looking sidebar menu build using ReactJS and styledComponents.It has really smooth animations.",
+  //   tags: ["react", "styledComponents", "react-router"],
+
+  //   demo: "https://react-sidebar.vercel.app/",
+  //   github: "https://github.com/codebucks27/react-sidebar",
+  // },
+  // {
+  //   id: 5,
+  //   name: "Todo App(React + Redux)",
+  //   description:
+  //     "A todo app build using Redux-toolkit and framer-motion for smooth animations.",
+  //   tags: ["react", "redux", "framer-motion"],
+
+  //   demo: "http://react-redux-todo-app-git-main-codebucks27.vercel.app/",
+  //   github: "https://github.com/codebucks27/React-Redux-Todo-App",
+  // },
+  // {
+  //   id: 6,
+  //   name: "Responsive Navigation Menu",
+  //   description:
+  //     "A reponsive navbar menu component in ReactJS with react-router functionalities. Built from scratch.",
+  //   tags: ["react", "react-router", "css"],
+
+  //   demo: "http://react-responsive-navbar-codebucks27.vercel.app/",
+  //   github: "https://github.com/codebucks27/React-responsive-navbar",
+  // },
+];
