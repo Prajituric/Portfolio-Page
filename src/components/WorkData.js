e7af88961cee1b0dcf03070174edeebfebed0beb
Prajituric <bugheanudanutandrei@gmail.com>
2022-09-05 19:11:48 +0300

    adding projects

--- src/components/WorkData.js
+++ src/components/WorkData.js
@@ -39,4 +39,24 @@ export const Work = [
     demo: "https://foodhealthy.netlify.app/",
     github: "https://github.com/Prajituric/Omnifood",
   },
+  {
+    id: 5,
+    name: "Travelly",
+    description:
+      "It is build as a prototype for a travel app. Based on css and html to combine the useful with the pleasant.",
+    tags: ["html", "css", "travel", "animations"],
+
+    demo: "https://travelwithlove.netlify.app/",
+    github: "https://github.com/Prajituric/Travelly",
+  },
+  {
+    id: 6,
+    name: "Photography",
+    description:
+      "A website which combine the photographer with the beauty of his art.",
+    tags: ["minimalism", "css"],
+
+    demo: "https://photographerantonia.netlify.app/",
+    github: "https://github.com/Prajituric/Photography-site",
+  },
 ];
